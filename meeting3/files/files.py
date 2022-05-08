8082d6531dfaa2930bf0a163d69474ee9842c86a
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-05-08 14:20:21 +0300

    adding files module

---
+++ meeting3/files/files.py
@@ -0,0 +1,25 @@
+if __name__ == "__main__":
+    realtive_path = "./data/alice_in_wonderland.txt"
+    absolute_path = "/Users/valeria/src/zeiss/meeting3/files/data/alice_in_wonderland.txt"
+
+    # read text file
+
+    # reading csv - basic
+
+    # write to file
+
+    # context manager
+
+    # reading from csv using DictReader
+
+
+
+
+
+
+
+# import csv
+# with open('/Users/valeria/src/zeiss/meeting3/files/data/AAPL.csv') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         print(row['Date'], row['Low'])
